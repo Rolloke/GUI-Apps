38283bd9128eef3e4c7e745b0d053116deac3785
Rolf <rolf-kary-ehlers@t-online.de>
2020-02-18 21:02:10 +0100

    Git Viewer files

---
+++ Qt/GitView/source/git_type.h
@@ -0,0 +1,79 @@
+#ifndef GIT_TYPE_H
+#define GIT_TYPE_H
+
+#include <boost/container/flat_map.hpp>
+#include <QString>
+
+namespace git
+{
+
+typedef boost::container::flat_map<int, QString> int2stringmap;
+
+struct Cmd
+{
+    Cmd();
+    enum eCmd
+    {
+        GetStatusAll,
+        ShowStatus,
+        ShowShortStatus,
+        ShowDifference,
+        Remove,
+        Add,
+        Commit,
+        /// hint: add commands here
+        CustomCommand=100
+    };
+    static const QString& getCommand(eCmd);
+private:
+    static int2stringmap mCommandMap;
+};
+
+struct Type
+{
+    enum eType
+    {
+        None        = 0,
+        File        = 0x0001,
+        Folder      = 0x0002,
+        Hidden      = 0x0004,
+        WildCard    = 0x0008,
+        GitModified = 0x0010,
+        GitDeleted  = 0x0020,
+        GitAdded    = 0x0040,
+        GitUnknown  = 0x0080,
+        GitIgnore   = 0x1000,
+        GitFolder   = 0x2000|Folder,
+        Repository  = 0x4000,
+        Checked     = 0x8000,
+        AllGitActions = GitModified|GitDeleted|GitAdded|GitUnknown
+    };
+
+    Type(uint aType = None, int aLevel = 0)
+      : mType(aType)
+      , mLevel(aLevel) {}
+
+    void add(eType aType);
+    void remove(eType aType);
+    bool is(eType aType);
+
+    static const char* name(eType aType);
+    static eType       translate(const QString& fIdentifier);
+
+    uint mType;
+    int mLevel;
+};
+
+namespace Folder
+{
+extern const char GitIgnoreFile[];
+extern const char GitRepository[];
+extern const char FolderUp[];
+extern const char FolderSelf[];
+}
+
+typedef boost::container::flat_map<std::string, Type> stringt2typemap;
+
+}
+
+#endif // GIT_TYPE_H
