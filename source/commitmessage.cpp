6454b9c20c3b452ee15ac6b709204c6dce159d4b
Rolf (Werkstatt) <rolf-kary-ehlers@t-online.de>
2020-02-24 21:19:30 +0100

    new commit dialog

---
+++ source/commitmessage.cpp
@@ -0,0 +1,25 @@
+#include "commitmessage.h"
+#include "ui_commitmessage.h"
+
+CommitMessage::CommitMessage(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CommitMessage)
+{
+    ui->setupUi(this);
+}
+
+CommitMessage::~CommitMessage()
+{
+    delete ui;
+}
+
+
+QString CommitMessage::getMessageText()
+{
+    return ui->textMessageEdit->toPlainText();
+}
+
+bool    CommitMessage::getAutoStage()
+{
+    return ui->ckAutoStage->isChecked();
+}
