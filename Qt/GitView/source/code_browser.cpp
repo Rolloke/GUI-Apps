d76255422b2f4d46ee6a5c2b078ffc685652df1d
Rolf Kary-Ehlers <rolf-kary-ehlers@t-online.de>
2021-11-27 21:03:57 +0100

    code browser class

---
+++ Qt/GitView/source/code_browser.cpp
@@ -0,0 +1,169 @@
+#include "code_browser.h"
+#include "logger.h"
+
+#include <QPainter>
+#include <QTextBlock>
+#include <QScrollBar>
+
+// NOTE: source for this solution, but a little bit modified
+// https://stackoverflow.com/questions/2443358/how-to-add-lines-numbers-to-qtextedit
+// https://doc.qt.io/qt-5/qtwidgets-widgets-codeeditor-example.html
+
+code_browser::code_browser(QWidget *parent): QTextBrowser(parent)
+  , m_line_number_area(new LineNumberArea(this))
+  , m_show_line_numbers(false)
+{
+    connect(document(), SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
+    connect(verticalScrollBar(), SIGNAL(valueChanged(int)), this, SLOT(vertical_scroll_value(int)));
+    connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
+    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
+
+    updateLineNumberAreaWidth(blockCount());
+    highlightCurrentLine();
+}
+
+int code_browser::lineNumberAreaWidth()
+{
+    if (m_show_line_numbers)
+    {
+        int digits = 1;
+        int max = qMax(1, blockCount());
+        while (max >= 10)
+        {
+            max /= 10;
+            ++digits;
+        }
+
+        int space = 3 + fontMetrics().horizontalAdvance(QLatin1Char('9')) * digits;
+
+        return space;
+    }
+    return 0;
+}
+
+void code_browser::set_show_line_numbers(bool show)
+{
+    m_show_line_numbers = show;
+    updateLineNumberAreaWidth(blockCount());
+    m_line_number_area->setVisible(show);
+}
+
+void code_browser::updateLineNumberAreaWidth(int)
+{
+    setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+}
+
+void code_browser::vertical_scroll_value(int value)
+{
+    if (m_show_line_numbers)
+    {
+        Q_EMIT updateRequest(contentsRect(), value);
+    }
+}
+
+void code_browser::updateLineNumberArea(const QRect &rect, int dy)
+{
+    if (dy)
+    {
+        m_line_number_area->scroll(0, dy);
+    }
+    else
+    {
+        m_line_number_area->update(0, rect.y(), m_line_number_area->width(), rect.height());
+    }
+    if (rect.contains(viewport()->rect()))
+    {
+        updateLineNumberAreaWidth(blockCount());
+    }
+}
+
+void code_browser::resizeEvent(QResizeEvent *e)
+{
+    QTextBrowser::resizeEvent(e);
+    QRect cr = contentsRect();
+    m_line_number_area->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+}
+
+void code_browser::highlightCurrentLine()
+{
+    QList<QTextEdit::ExtraSelection> extraSelections;
+
+    if (!isReadOnly())
+    {
+        QTextEdit::ExtraSelection selection;
+
+        QColor lineColor = QColor(Qt::yellow).lighter(160);
+
+        selection.format.setBackground(lineColor);
+        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
+        selection.cursor = textCursor();
+        selection.cursor.clearSelection();
+        extraSelections.append(selection);
+    }
+
+    setExtraSelections(extraSelections);
+}
+
+void code_browser::lineNumberAreaPaintEvent(QPaintEvent *event)
+{
+    QPainter painter(m_line_number_area);
+    painter.fillRect(event->rect(), Qt::lightGray);
+
+    int top = 0;
+    QTextBlock block = firstVisibleBlock(top);
+    int blockNumber = block.blockNumber();
+
+    QRectF block_rect = blockBoundingRect(block);
+    int bottom = top + qRound(block_rect.height());
+    while (block.isValid() && top <= event->rect().bottom())
+    {
+        if (block.isVisible() && bottom >= event->rect().top())
+        {
+            QString number = QString::number(blockNumber + 1);
+            painter.setPen(Qt::black);
+            painter.drawText(0, top, m_line_number_area->width(), fontMetrics().height(), Qt::AlignRight, number);
+        }
+
+        block = block.next();
+        top = bottom;
+        block_rect = blockBoundingRect(block);
+        bottom = top + qRound(block_rect.height());
+        ++blockNumber;
+    }
+}
+
+int code_browser::blockCount() const
+{
+    return document()->blockCount();
+}
+
+QTextBlock code_browser::firstVisibleBlock(int& diff)
+{
+    QPointF content_offset = contentOffset();
+    for (QTextBlock block = document()->begin(); block.isValid(); block = block.next())
+    {
+        if (block.isVisible())
+        {
+            QRectF block_rect = blockBoundingRect(block);
+            if (block_rect.top() >= content_offset.y())
+            {
+                diff = block_rect.top() - content_offset.y();
+                return block;
+            }
+        }
+    }
+    diff = -1;
+    return document()->begin();
+}
+
+//! Returns the bounding rectangle of the text block in the block's own coordinates.
+QRectF code_browser::blockBoundingRect(const QTextBlock &block) const
+{
+    QAbstractTextDocumentLayout *layout = document()->documentLayout();
+    return layout->blockBoundingRect(block);
+}
+
+QPointF code_browser::contentOffset() const
+{
+    return QPointF(horizontalScrollBar()->value(), verticalScrollBar()->value());
+}
